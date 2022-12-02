4badaa90faa6e102a0a9c61df7e5aebf09c762a0
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-12-03 00:20:46 +0530

    add passing tests

--- tests/unitary/test_get_state_variables.py
+++ tests/unitary/test_get_state_variables.py
@@ -3,11 +3,21 @@ import pytest
 import boa
 
 source_code = """
-A: constant(uint256) = 10
+A: public(constant(uint256)) = 10
+B: constant(uint256) = 20
 
 @external
 def __init__():
     pass
+
+@external
+def show() -> uint256:
+    return B
+
+@external
+@payable
+def show_payable() -> uint256:
+    return B
 """
 
 
@@ -17,6 +27,19 @@ def contract():
     return c
 
 
-def test_get_constant_var(contract):
+def test_show(contract):
+    assert contract.show() == 20
+
+
+def test_show_payable(contract):
+    assert contract.show_payable() == 20
+
+
+def test_get_public_constant_var(contract):
     A = contract.eval("A")
     assert A == 10
+
+
+def test_get_constant_var(contract):
+    A = contract.eval("B")
+    assert A == 20
